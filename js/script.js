73cdd16c836a7f4e3f723c858d6c6160ab7e3564
Victor Matos <antoniovitomatos@gmail.com>
2022-03-04 20:21:30 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 const key =  "b5419f679f98edf4ad4be4d55d04c894";
-let long, lat;
-
+let lat = 0;
+let long;
 function obterData(){
-    let long = document.querySelector(".long").value;
-    let lat = document.querySelector(".lat").value;
+    long = document.querySelector(".long").value;
+    lat = document.querySelector(".lat").value;
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`
     const promise = axios.get(url);
     promise.then(imprimirData)
 }
 
 function imprimirData(load){
-    console.log(load.data.weather[0]);
+    console.log(load.data.weather[0].icon);
     let data = document.querySelector(".conteudo")
     let temperatura = load.data.main.temp - 273.15;
+    const urlIcon = ` http://openweathermap.org/img/wn/10d@2x.png`;
     data.innerHTML=`
     <ul>
         <li>${load.data.name}</li>
@@ -20,5 +21,18 @@ function imprimirData(load){
         <li>${load.data.weather[0].main}</li>
         <li>${load.data.weather[0].description}</li>
     </ul>
+    <img src="http://openweathermap.org/img/wn/${load.data.weather[0].icon}@2x.png" alt=""> 
 `
+document.querySelector(".all").style.backgroundImage = `url('./content/${load.data.weather[0].description}.jpg')`;
 }
+function busca(callback) { 
+    navigator.geolocation.getCurrentPosition(function(position) {
+        lat = position.coords.latitude;
+        long= position.coords.longitude;
+        callback();
+        });
+}
+function printLL(){
+    console.log(lat, long);
+}
+busca(printLL);
